42cdf9de33bab0b9283eac89533ef6f7b73e1b45
Kenneth P Chang <kennethpchang@gmail.com>
2021-06-27 16:52:50 -0700

    two props: The current list of contacts and a callback function for adding a new contact

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+export const ContactsPage = (props) => {
+  // Define state variables for contact info and duplicate check
+  // implement ContactsPage as a stateful component to handle the logic for adding new contacts and listing current contacts.
+  // Receive two props: The current list of contacts and a callback function for adding a new contact
+  const contacts = props.contacts;
+  const addContact = props.addContact;
+
+  // const [contactInfo, setContactInfo] = useState();
+  // const [duplicateCheck, setDuplicateCheck] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault();
