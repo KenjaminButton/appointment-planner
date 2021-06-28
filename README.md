b4332d727d57f87ca5a80a1d1af311d0f2146bb6
Kenneth P Chang <kennethpchang@gmail.com>
2021-06-27 18:02:40 -0700

    ContactPage.js state setup completed, but still needs work.

--- README.md
+++ README.md
@@ -10,4 +10,19 @@
 
 5. Passed the appointments array, contacts array, and the add appointment function as props to the AppointmentsPage component
 
-6. 
+6. Receive two props:
+The current list of contacts
+A callback function for adding a new contact
+
+7. Keep track of three local state values: the current name, phone, and email entered into the form
+
+8. Check for duplicates whenever the name in the form changes and indicate the name is a duplicate
+
+9. Only add a new contact on form submission if it does not duplicate an existing contact’s name
+
+10. A successful submission should clear the form
+
+11. In the Add Contact section, render a ContactForm with the following passed via props:
+local state variables
+local state variable setter functions
+handleSubmit callback function
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,21 +1,52 @@
 import React, {useState} from "react";
+import {ContactForm} from '../../components/contactForm/ContactForm';
+import {TileList} from '../../components/tileList/TileList';
 
 export const ContactsPage = (props) => {
-  // Define state variables for contact info and duplicate check
-  // implement ContactsPage as a stateful component to handle the logic for adding new contacts and listing current contacts.
-  // Receive two props: The current list of contacts and a callback function for adding a new contact
+  /*
+  implement ContactsPage as a stateful component to handle the logic for adding
+  new contacts and listing current contacts. Keep track of three local state
+  values: the current name, phone, and email entered into the form
+  Define state variables for contact info (name, phone and email)
+  */
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  /*
+  Set state variable for duplicate check with a default value to false
+  Check for duplicates whenever the name in the form changes and indicate
+  the name is a duplicate
+  */
+  const [duplicate, setDuplicate] = useState(false);
+
+  /*
+  Receive two props: The current list of contacts and a callback
+  function for adding a new contact
+  */
   const contacts = props.contacts;
   const addContact = props.addContact;
 
-  // const [contactInfo, setContactInfo] = useState();
-  // const [duplicateCheck, setDuplicateCheck] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
+    Add contact info
     */
+    addContact(name, phone, email);
+    
+    /*
+    If the contact name is not a duplicate
+    Only add a new contact on form submission if it
+    does not duplicate an existing contact’s name
+    */
+    if (!duplicate) {
+      // Clear data
+      // A successful submission clearing the form
+      setName('');
+      setPhone('');
+      setEmail('');
+    }
+    
   };
 
   /*
@@ -26,11 +57,29 @@ export const ContactsPage = (props) => {
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        {/* Rendering a ContactForm with the following props: local state
+        variables, local state variable setter functions, and handleSubmit
+        callback function */}
+        <ContactForm 
+          name={name}
+          phone={phone}
+          email={email}
+          setName={setName}
+          setPhone={setPhone}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
+
+
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        {/* In the Contacts section, render a TileList with the contact array passed via props */}
+        <TileList 
+          objArr={props.contacts}
+        />
       </section>
     </div>
   );
